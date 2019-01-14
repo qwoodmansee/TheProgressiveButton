dc7c827ee985aa96be72b3b9d94861cafbe00c37
Quinton Woodmansee <quinton.woodmansee@joinroot.com>
2019-01-14 10:50:23 -0500

    setup new deploy scripts for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const encore = require('@symfony/webpack-encore');
 
 encore
-    .setOutputPath('web/TheProgressiveButton/')
-    .setPublicPath('/TheProgressiveButton/')
+    .setOutputPath('web/build/')
+    .setPublicPath('/build')
     .addEntry('app', './assets/js/app.js')
     .enableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!encore.isProduction())
-    .enableVersioning(encore.isProduction())
+    //.enableVersioning(encore.isProduction())
     .enableSassLoader()
     .enableReactPreset();
 
-console.log(encore.getWebpackConfig());
 module.exports = encore.getWebpackConfig();
