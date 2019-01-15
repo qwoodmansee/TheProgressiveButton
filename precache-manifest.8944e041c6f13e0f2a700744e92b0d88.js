4a074a5cee40501a34fc07bac02575e6198dc486
Quinton Woodmansee <quinton.woodmansee@joinroot.com>
2019-01-15 08:46:39 -0500

    gh-pages update

---
+++ precache-manifest.8944e041c6f13e0f2a700744e92b0d88.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0acccb39cd5d81db190827806c14168",
+    "url": "/images/icons/icon-96x96.png"
+  },
+  {
+    "url": "/icon_72x72.c5e4f8e1a9a8685be3e058fc487b87a3.png"
+  },
+  {
+    "revision": "2b67a6d911a20a4ef804",
+    "url": "/runtime.js"
+  },
+  {
+    "revision": "e5dd95c91b3daf2c57fe13ad8838744d",
+    "url": "/images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "f26c549f6df13902b84deb6b35ddc844",
+    "url": "/images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "2d338a19671a955d8e4d0aeee37bb273",
+    "url": "/images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "a76961ad8316322c3fa81496bd877bdf",
+    "url": "/images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "36d74f1cd5069945574cff40db456a9f",
+    "url": "/images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "f26c549f6df13902b84deb6b35ddc844",
+    "url": "/images/icons/icon.png"
+  },
+  {
+    "revision": "1b949da7d74d965275b61a605d403852",
+    "url": "/images/icons/icon-152x152.png"
+  },
+  {
+    "url": "/icon_96x96.3828f8f19afbbf9721346379c814a073.png"
+  },
+  {
+    "revision": "eb873ee975b0cc08653533264a8f37fd",
+    "url": "/images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "224679739fa89a76a9facd9c29fffd43",
+    "url": "/index.html"
+  },
+  {
+    "url": "/icon_512x512.1fb15cd8055054587e87e095f1a8d65e.png"
+  },
+  {
+    "url": "/icon_384x384.1b8831d25b221fe04b560a571deb668a.png"
+  },
+  {
+    "url": "/icon_192x192.2fd551e7d873acb8684d84374cb99fa0.png"
+  },
+  {
+    "url": "/icon_152x152.a5fb88cf95a72cfa088327070a15713c.png"
+  },
+  {
+    "url": "/icon_144x144.e233df9620326a5e15a10c19b45c7ef9.png"
+  },
+  {
+    "url": "/icon_128x128.953bc12ea7c2966907111e9426ec6c61.png"
+  },
+  {
+    "revision": "89305a363aa8d6f1e4fd",
+    "url": "/app.js"
+  },
+  {
+    "revision": "89305a363aa8d6f1e4fd",
+    "url": "/app.css"
+  }
+];
